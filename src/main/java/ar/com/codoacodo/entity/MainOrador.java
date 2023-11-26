22b547a13350fd097b3685f05250e5c9ca7ae6c7
pabloandimu <pablo.mujica.trial@gmail.com>
2023-11-26 02:27:43 -0300

    02.27

--- src/main/java/ar/com/codoacodo/entity/MainOrador.java
+++ src/main/java/ar/com/codoacodo/entity/MainOrador.java
@@ -7,7 +7,7 @@ public class MainOrador {
 	public static void main(String[] args) {
 		//crear un objeto de la Clase Orador, que luego se enviara a la db
 		Orador nuevoOrador = new Orador("carlos", "lopez", "email@email.com", "Java", LocalDate.now());
-		//comentario nuevo
+		//comentario nuevo no agregado
 		System.out.println(nuevoOrador);
 		
 	}
